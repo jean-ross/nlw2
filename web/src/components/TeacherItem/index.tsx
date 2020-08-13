e087d93464c1cef1c4633d69e34c2a4a0408c31a
Jean Carlos Martins Ross <jeanross@MacBook-Pro-de-Jean.local>
2020-08-12 21:45:29 -0300

    Added API integration

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,43 +2,59 @@ import React from "react";
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from "../../services/api";
+
 import "./styles.css";
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+  
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://pbs.twimg.com/profile_images/1078278632290832386/nOeGNCpC_400x400.jpg"
-          alt="Goku"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Goku</strong>
-          <span>Kamehameha</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Goku é o personagem que mais possui conhecimento sobre técnicas de
-        batalha.
-        <br />
-        <br />
-        Os Saiyajins são uma raça guerreira, e uma de suas habilidades especiais
-        é a de se tornarem mais fortes após serem feridos gravemente, e se
-        chegarem ao ponto de quase morrer devido a lesões físicas graves e se
-        recuperarem, seu poder de luta aumentará incrivelmente o que é chamado
-        de zenkai.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 90,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/55${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Entrar em contato" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
